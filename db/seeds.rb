3053cbf1127a203003ec222e4cbd66f2f4bf826f
Emma Gardner <emmagardner@outlook.com>
2014-08-22 10:44:46 -0400

    added some seed data to seeds file and cleaned up gemfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,213 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+plant.create(
+  image:,
+  description: "An heirloom tomato that tastes great!",
+  sunlight: "Full sun: 6 - 8+ hours a day",
+  water: "1 - 2 inches/week",
+  grow_season: "Spring- after last frost",
+  height: 45,
+  width: 24,
+  spacing: 24,
+  seed_depth: 0.25,
+  days_to_maturity: 90,
+  soil_reqs: "well-drained with lots of organic matter",
+  name: "Brandywine Tomato"
+  )
+plant.create(
+  image:,
+  description: "A hearty green that grows well in cooler weather and even after frosts.",
+  sunlight: "Partial sun",
+  water: "1 - 2 inches/week",
+  grow_season: "4 - 6 weeks before last frost in spring and 4-6 weeks before first frostin fall" ,
+  height: 10,
+  width: 8,
+  spacing: 8,
+  seed_depth: 0.5,
+  days_to_maturity: 40,
+  soil_reqs: "well-drained with lots of organic matter",
+  name: 'Spinach'
+  )
+plant.create(
+  image:,
+  description: ,
+  sunlight: ,
+  water: ,
+  grow_season: ,
+  height: ,
+  width: ,
+  spacing: ,
+  seed_depth: ,
+  days_to_maturity: ,
+  soil_reqs: ,
+  name:
+  )
+plant.create(
+  image:,
+  description: ,
+  sunlight: ,
+  water: ,
+  grow_season: ,
+  height: ,
+  width: ,
+  spacing: ,
+  seed_depth: ,
+  days_to_maturity: ,
+  soil_reqs: ,
+  name:
+  )
+plant.create(
+  image:,
+  description: ,
+  sunlight: ,
+  water: ,
+  grow_season: ,
+  height: ,
+  width: ,
+  spacing: ,
+  seed_depth: ,
+  days_to_maturity: ,
+  soil_reqs: ,
+  name:
+  )
+plant.create(
+  image:,
+  description: ,
+  sunlight: ,
+  water: ,
+  grow_season: ,
+  height: ,
+  width: ,
+  spacing: ,
+  seed_depth: ,
+  days_to_maturity: ,
+  soil_reqs: ,
+  name:
+  )
+plant.create(
+  image:,
+  description: ,
+  sunlight: ,
+  water: ,
+  grow_season: ,
+  height: ,
+  width: ,
+  spacing: ,
+  seed_depth: ,
+  days_to_maturity: ,
+  soil_reqs: ,
+  name:
+  )
+plant.create(
+  image:,
+  description: ,
+  sunlight: ,
+  water: ,
+  grow_season: ,
+  height: ,
+  width: ,
+  spacing: ,
+  seed_depth: ,
+  days_to_maturity: ,
+  soil_reqs: ,
+  name:
+  )
+plant.create(
+  image:,
+  description: ,
+  sunlight: ,
+  water: ,
+  grow_season: ,
+  height: ,
+  width: ,
+  spacing: ,
+  seed_depth: ,
+  days_to_maturity: ,
+  soil_reqs: ,
+  name:
+  )
+plant.create(
+  image:,
+  description: ,
+  sunlight: ,
+  water: ,
+  grow_season: ,
+  height: ,
+  width: ,
+  spacing: ,
+  seed_depth: ,
+  days_to_maturity: ,
+  soil_reqs: ,
+  name:
+  )
+plant.create(
+  image:,
+  description: ,
+  sunlight: ,
+  water: ,
+  grow_season: ,
+  height: ,
+  width: ,
+  spacing: ,
+  seed_depth: ,
+  days_to_maturity: ,
+  soil_reqs: ,
+  name:
+  )
+plant.create(
+  image:,
+  description: ,
+  sunlight: ,
+  water: ,
+  grow_season: ,
+  height: ,
+  width: ,
+  spacing: ,
+  seed_depth: ,
+  days_to_maturity: ,
+  soil_reqs: ,
+  name:
+  )
+plant.create(
+  image:,
+  description: ,
+  sunlight: ,
+  water: ,
+  grow_season: ,
+  height: ,
+  width: ,
+  spacing: ,
+  seed_depth: ,
+  days_to_maturity: ,
+  soil_reqs: ,
+  name:
+  )
+plant.create(
+  image:,
+  description: ,
+  sunlight: ,
+  water: ,
+  grow_season: ,
+  height: ,
+  width: ,
+  spacing: ,
+  seed_depth: ,
+  days_to_maturity: ,
+  soil_reqs: ,
+  name:
+  )
+plant.create(
+  image:,
+  description: ,
+  sunlight: ,
+  water: ,
+  grow_season: ,
+  height: ,
+  width: ,
+  spacing: ,
+  seed_depth: ,
+  days_to_maturity: ,
+  soil_reqs: ,
+  name:
+  )
