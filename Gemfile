489077040e21b66cb3eec231eb760f835e87e9fc
Emma Gardner <emmagardner@outlook.com>
2014-08-22 11:58:13 -0400

    added user model with devise

--- Gemfile
+++ Gemfile
@@ -15,3 +15,4 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 
 gem 'spring',        group: :development
 
+gem 'devise'
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140822151437) do
+ActiveRecord::Schema.define(version: 20140822155530) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "custom_plants", force: true do |t|
+    t.text     "name"
+    t.string   "image"
+    t.string   "description"
+    t.text     "sun"
+    t.text     "water"
+    t.text     "growing_season"
+    t.integer  "height"
+    t.integer  "width"
+    t.integer  "spacing"
+    t.decimal  "seed_depth"
+    t.integer  "days_to_maturity"
+    t.text     "soil_reqs"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "gardens", force: true do |t|
+    t.text     "name"
+    t.text     "location"
+    t.string   "notes"
+    t.boolean  "rain_alert"
+    t.boolean  "frost_alert"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "plants", force: true do |t|
     t.text     "name"
@@ -32,5 +59,23 @@ ActiveRecord::Schema.define(version: 20140822151437) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
