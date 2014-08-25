d3facad39ed78b65e041a3e455ffc71276d02027
Emma Gardner <emmagardner@outlook.com>
2014-08-25 14:48:21 -0400

    raked migrations from master pull

--- db/schema.rb
+++ db/schema.rb
@@ -11,39 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20140825154751) do
-=======
-ActiveRecord::Schema.define(version: 20140822174954) do
->>>>>>> 864cdbf1b0a4856342a74ed2781fd92d57513fb5
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-<<<<<<< HEAD
   create_table "garden_plants", id: false, force: true do |t|
     t.integer  "garden_id"
     t.integer  "plant_id"
     t.datetime "planted_on"
     t.text     "notes"
-=======
-  create_table "custom_plants", force: true do |t|
-    t.text     "name"
-    t.string   "image"
-    t.string   "description"
-    t.text     "sun"
-    t.text     "water"
-    t.text     "growing_season"
-    t.integer  "height"
-    t.integer  "width"
-    t.integer  "spacing"
-    t.decimal  "seed_depth"
-    t.integer  "days_to_maturity"
-    t.text     "soil_reqs"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "garden_id"
->>>>>>> 864cdbf1b0a4856342a74ed2781fd92d57513fb5
   end
 
   create_table "gardens", force: true do |t|
