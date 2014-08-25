9dad54eca05bd279450fdfa9bca8fa2d6d9c7c83
sarahchristine913 <s.mele13@gmail.com>
2014-08-25 15:42:31 -0400

    App has updated schema
    + Dropped join table (incorrect HABTM association)
    + New join table with proper relations
    + Updated associations in plants, user id replaces garden id

--- db/migrate/20140825193450_create_garden_plants.rb
+++ db/migrate/20140825193450_create_garden_plants.rb
@@ -1,8 +1,9 @@
 class CreateGardenPlants < ActiveRecord::Migration
   def change
+    drop_table :gardens_plants
     create_table :garden_plants, id: false do |t|
-      t.references :garden
-      t.references :plant
+      t.references :gardens
+      t.references :plants
       t.datetime :planted_on
       t.text :notes
     end
